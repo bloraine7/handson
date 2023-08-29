fc8d355e70dc5bc7d687270a1bbcab7a0a21c5d1
Aluno <aluno@edu.senac.br>
2023-08-28 21:41:24 -0300

    meu primeiro commit

---
+++ js/handson02.js
@@ -0,0 +1,7 @@
+function exibirMensagem(){
+    var nome = document.getElementById("nome").value;
+    var sobrenome = document.getElementById("sobrenome").value;
+
+    alert("Hello World, "+ nome + " " + sobrenome);
+    alert(`Hello World ${nome + " " + sobrenome }`);
+}  
---
+++ js/handson03.js
@@ -0,0 +1,11 @@
+function somar(){
+    /*entrada*/
+    var num1 = parseInt(document.getElementById("num1").value)
+    var num2 = parseInt(document.getElementById("num2").value)
+
+    /*processamento*/
+    var soma = num1 + num2
+
+    /*saida*/
+    alert(num1+num2)
+}
